1b71c4050b54ee08877046e0abe768e873cc5436
brucewave <longthanh.dev@gmail.com>
2024-10-02 17:14:27 +0700

    setup navigation and tab

---
+++ src/screens/navigators/TabNavigator.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import HomeScreen from '../home/HomeScreen';
+
+const TabNavigator = () => {
+
+    const Tab = createBottomTabNavigator();
+
+  return (
+   <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Screen name='HomeScreen' component={HomeScreen}>
+    </Tab.Screen>
+   </Tab.Navigator>
+  )
+}
+
+export default TabNavigator

bb8e9e1c930a045f2b6983ac30b606b5f4fb6799
brucewave <longthanh.dev@gmail.com>
2024-12-15 20:27:14 +0700

    convert to expo & build base

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import TextComponent from "./TextComponent";
-import SpaceComponent from "./SpaceComponent";
-import ButtonComponent from "./ButtonComponent";
-import InputComponent from "./InputComponent";
-import SectionComponent from "./SectionComponent";
-import ContainerComponent from "./ContainerComponent";
-import RowComponent from "./RowComponent";
-
-export{SpaceComponent,TextComponent,ButtonComponent, InputComponent, SectionComponent, ContainerComponent, RowComponent};
--- src/constants/fontFamilies.ts
+++
@@ -1,6 +0,0 @@
-export const fontFamilies = {
-    regular: 'BalooPaaji2-Regular',
-    medium: 'BalooPaaji2-Medium',
-    semiBold: 'BalooPaaji2-SemiBold',
-    bold: 'BalooPaaji2-Bold',
-  };
